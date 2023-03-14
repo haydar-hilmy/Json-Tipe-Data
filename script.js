33e9cb037a11d734a949b87d70f6ed20607bc13f
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2023-03-14 23:34:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     $.get(myURL, function (data) {
         console.log("Use Get Jquery: ", data);
         console.log("Nama index ke 1: ", data.nama[1]);
-        console.log("Object didalam Object: ", data.nama[5].subnama);
+        console.log("Object didalam Object: ", data.nama[7].subnama);
         console.log("Print All:");
         for(let i = 0; i < data.nama.length; i++){
             console.log("Data ke-", i, "tipe data:", typeof data.nama[i], "isi data:", data.nama[i]);
