81c7b54269f69986a5ee2da9f7cd48cd1f7a6ae5
Haydar Hilmy <88047909+haydar944@users.noreply.github.com>
2023-03-14 23:16:04 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const myURL = "json001.json";
+
+fetch(myURL)
+    .then(response => response.json())
+    .then(data => {
+        console.log("Use Fetch: ", data);
+    })
+
+$(document).ready(function () {
+    $.get(myURL, function (data) {
+        console.log("Use Get Jquery: ", data);
+        console.log("Nama index ke 1: ", data.nama[1]);
+        console.log("Object didalam Object: ", data.nama[5].subnama);
+        console.log("Print All:");
+        for(let i = 0; i < data.nama.length; i++){
+            console.log("Data ke-", i, "tipe data:", typeof data.nama[i], "isi data:", data.nama[i]);
+        }
+    });
+})
